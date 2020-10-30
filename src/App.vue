32ea474f8266b75da96c5425bfc19e4d931f3b6d
Harshil Shah <hash_1492@yahoo.com>
2020-10-30 14:22:23 -0400

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,23 @@ export default {
   components: {
     Shell
   },
+  metaInfo: {
+    title: 'Mansi Creative Corner',
+    meta: [
+      { name: 'description', content: 'Beautiful handmade jewellery like necklaces, bangles, earrings and more!' },
+      { property: 'og:title', content: 'Mansi Creative Corner' },
+      { property: 'og:description', content: 'Beautiful handmade jewellery like necklaces, bangles, earrings and more!' },
+      { property: 'og:image', content: 'https://firebasestorage.googleapis.com/v0/b/creative-corner-8ced8.appspot.com/o/default.png?alt=media&token=0188b1b4-e2d2-4e0b-8b5e-5a903843caab' },
+      { property: 'og:image:type', content: 'image/png' },
+      { property: 'og:image:width', content: '600' },
+      { property: 'og:image:height', content: '600' },
+      { property: 'og:url', content: 'https://www.mansicreativecorner.com' },
+      { property: 'og:type', content: 'content' }
+    ],
+    link: [
+      { rel: 'shortcut icon', href: 'favicon.ico', type:'image/x-icon' },
+    ]
+  }
 }
 </script>
 
